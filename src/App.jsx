ec9ebd5c0ef7463786d7b7fc496b74ff27f7a808
Francisco de Assis <a124fr@gmail.com>
2022-05-17 00:30:03 -0300

    Ajustando o Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from './components/layout/Card';
+import './App.css';
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametros from './components/basicos/ComParametros';
@@ -9,25 +10,25 @@ import Aleatorio from './components/basicos/Aleatorio';
 
 // A Forma mais reduzida e uma função	
 export default () => (
-	<div>
+	<div class="App">
 		<h1>Fundamentos React(Arrow)</h1>
-		<Card titulo="Desafios Aleatório">
-			<Aleatorio min={10} max={60} />
-		</Card>
-		<Card titulo="Fragmento">
-			<Fragmento />
-		</Card>
+		<div className='Cards'>			
+				<Card titulo="Desafios Aleatório">
+					<Aleatorio min={10} max={60} />
+				</Card>
+				<Card titulo="Fragmento">
+					<Fragmento />
+				</Card>
 
-		<Card titulo="Com Parametro">
-			<ComParametros
-				titulo="Situação do Aluno"
-				aluno="Pedro" nota={3} />
-		</Card>
-		
-		<Card titulo="Primeiro Componente">
-			<Primeiro></Primeiro>
-		</Card>
-		
-		
+				<Card titulo="Com Parametro">
+					<ComParametros
+						titulo="Situação do Aluno"
+						aluno="Pedro" nota={3} />
+				</Card>
+				
+				<Card titulo="Primeiro Componente">
+					<Primeiro></Primeiro>
+				</Card>
+			</div>		
 	</div>
 );
